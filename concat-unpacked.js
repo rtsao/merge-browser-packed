da0ccc096ae8820e981326be31375e8f14470080
Ryan Tsao <rtsao@uber.com>
2016-03-18 15:33:37 -0700

    Added dupe source handling

--- concat-unpacked.js
+++ concat-unpacked.js
@@ -9,12 +9,22 @@ module.exports = function concatUnpacked(files) {
   });
 }
 
+var regex = /^arguments\[4\]\[(\d+)\]\[0\]\.apply\(exports,arguments\)$/;
+
 function offsetRowIds(row, offset) {
   // update row id
   row.id += offset;
+
   // update ids of deps
   Object.keys(row.deps).forEach(function(dep) {
     row.deps[dep] += offset;
   });
+
+  // update dupe source if necessary
+  var matches = regex.exec(row.source);
+  if (matches && matches[1]) {
+    row.source = 'arguments[4][' + row.deps.dup + '][0].apply(exports,arguments)';
+  }
+
   return row;
 }
